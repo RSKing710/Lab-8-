99d893e21bcd2fe4b6dc1e6104684d773f667908
RSKing710 <52184331+RSKing710@users.noreply.github.com>
2021-04-06 20:26:00 -0400

    Add files via upload

--- hash.py
+++ hash.py
@@ -27,7 +27,7 @@ def traverseOS():
 		for file in files: 
 			path = os.path.join(subdir, file)
 			if path.startswith('/dev') or path.startswith('/proc') or path.startswith('/run') or path.startswith('/sys') or path.startswith('/tmp') or path.startswith('/var/lib') or path.startswith('/var/run'): 
-				continue
+				continue		#ignore these directories 
 			listOfFiles.append(path)
 			print(path)    
 	return listOfFiles        
@@ -43,44 +43,47 @@ def getFileInfo(listOfFiles):
 				for byte in iter(lambda: rf.read(4096), b""):
 					hashf.update(byte)
 				print("File content's hash: " + hashf.hexdigest())
-			val = [hashf.hexdigest(), time]
-			fileDict[files] = val
+			val = [hashf.hexdigest(), time]	#hashes file 
+			fileDict[files] = val #stores value in dict as a tuple of hash and timestamp
 		except:
 			print("Exception occured")
 	return fileDict
 
 
-def printData(dictionary): 
+def printData(dictionary): #writes data to storedhash.txt
 	with open('storedhash.txt', 'w+') as f: 
 		for k,v in dictionary.items():
 			f.write(f'{k}:{v}\n')
 
 def getData(file): 
 	dictionary = {}
-	with open(file, "r") as f: 
-		for line in f: 
-			line = line.split(':')
-			key = line[0]
-			values = line[1].rstrip().split(',')
-			values = [values[0], [values[1]]]
-			hashVal = values[0][1:].replace('\'', '')
-			timeVal = values[1][0].replace(']', '').replace(' ', '')
-			vals = [hashVal, timeVal]
-			dictionary[key] = vals
+	with open(file, "r") as f: #grabbing each files name as key and hash and timestamp as value
+		for line in f:
+			try:  
+				line = line.split(':')
+				key = line[0]
+				values = line[1].rstrip().split(',')	#cleaning up dating so that it matches original dictionary 
+				values = [values[0], [values[1]]]
+				hashVal = values[0][1:].replace('\'', '')
+				timeVal = values[1][0].replace(']', '').replace(' ', '')
+				vals = [hashVal, timeVal]
+				dictionary[key] = vals
+			except: 
+				continue
 		return dictionary 
 
 def compareData(dict1, dict2): 
 	newFiles = {}
 	missingFiles = {}
 	modifiedFiles = {}
-	og_keys = set(dict1.keys())
-	new_keys = set(dict2.keys())
+	og_keys = set(dict1.keys())	#put old keys in a set
+	new_keys = set(dict2.keys())	#put new keys in a set
 	shared_keys = og_keys.intersection(new_keys) 
 
-	removedFiles = og_keys - new_keys
-	newFiles = new_keys - og_keys 
+	removedFiles = og_keys - new_keys	#any keys missing = removed file
+	newFiles = new_keys - og_keys 		#any keys added = added file
 	
-	for x in shared_keys:
+	for x in shared_keys:	#checks to see if hash is different, if it is then the file is added to modified files dict. 
 		if dict1[x][0] != dict2[x][0]: 
 			modifiedFiles[x] = [dict1[x][0], dict2[x][0]]	
 
@@ -99,24 +102,29 @@ def usage():
 
 def main():
 	try: 
-		opts, args = getopt.getopt(sys.argv[1:], "iu", ["initialScan", "updateScan"])
+		opts, args = getopt.getopt(sys.argv[1:], "iu", ["initialScan", "updateScan"])	#use getopt to either run initial scan or updated scan looking for new, modified, or deleted files
 	except getopt.GetoptError as err: 
 		print(err)
 		usage()
+
+	if len(sys.argv) != 2: 
+		usage()
+
 	for o, a in opts: 
 		if o in ("-i", "--initialScan"): 
 			listOfFiles = traverseOS()
 			dictionary = getFileInfo(listOfFiles)
 			printData(dictionary)
+			exit()
 		elif o in ("-u", "--updateScan"): 
 			originalFiles = getData("storedhash.txt")
 			listOfFiles = traverseOS()
 			dictionary = getFileInfo(listOfFiles)
 			compareData(originalFiles, dictionary)
 			printData(dictionary)
+			exit()
 		else: 
 			usage()
-	usage()
 
 if __name__ == "__main__":
 	main()
